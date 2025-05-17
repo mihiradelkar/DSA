a29f0c40bb2b74532057a0f68163fd7b9049bdc0
Mihir Adelkar <adelkar.m@northeastern.edu>
2025-05-17 04:30:24 -0400

    day 2.1

--- 122_Best Time to Buy and Sell Stock II/index.js
+++ 122_Best Time to Buy and Sell Stock II/index.js
@@ -41,3 +41,13 @@ var maxProfit = function (prices) {
 
   return profit;
 };
+// simple solution
+var maxProfit = function (prices) {
+  let profit = 0;
+  for (i = 0; i < prices.length; i++) {
+    if (prices[i] < prices[i + 1]) {
+      profit = profit + (prices[i + 1] - prices[i]);
+    }
+  }
+  return profit;
+};
