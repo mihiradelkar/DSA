6c563011cb92e44c124343ed7f1015a39686368c
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-17 10:41:59 -0400

    Time: 35 ms (95.87%) | Memory: 20.7 MB (94.44%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,34 +1,6 @@
 class Solution:
     def calculate(self, s: str) -> int:
         # 3+3+2*2
-        num = 0
-        op = "+"
-        # res = []
-        total = 0
-        prev = 0
-        for ch in s + "+":
-            if ch.isdigit():
-                num = num * 10 + int(ch)
-            elif ch in "+-*/":
-                if op == "+":
-                    total += prev
-                    prev = num
-                    # res.append(num)
-                if op == "-":
-                    total += prev
-                    prev = -num
-                    # res.append(-num)
-                if op == "*":
-                    prev =  prev * num
-                    # res[-1]= res[-1] * num
-                if op == "/": 
-                    prev = int(prev / num)
-                    # res[-1]= int(res[-1] / num)
-                op = ch
-                num = 0
-        return total+prev #sum(res)
-
-
 # questions:
 # will the string be empty? yes return 0
 # what functions will it support? +-*/
@@ -76,36 +48,10 @@ class Solution:
                     prev = int(prev/num)
                 num=0
                 op=ch
-        # return sum(res)
-        return res+prev
+        return res+prev # sum(res)
 
-
-# Example 1:
-# Input: s = "3+2*2+"
-# Output: 7
-# res = [3,4]
-# op = *
-# res = 7
-
-        # optimized approach
-        # stack = []
-        # num = 0
-        # op = "+"
-        # # " 3/2 "
-        # for ch in s + "+":
-        #     if ch.isdigit():
-        #         num = num * 10 + int(ch)
-        #     if ch in "+-*/":
-        #         if op == "+":
-        #             stack.append(num)
-        #         elif op == "-":
-        #             stack.append(-num)
-        #         elif op == "*":
-        #             # stack.append(stack.pop()*num)
-        #             stack[-1] *= num
-        #         elif op == "/":
-        #             # stack.append(int(stack.pop()/num))
-        #             stack[-1] = int(stack[-1]/num)
-        #         op = ch
-        #         num = 0
-        # return sum(stack)
+# Input: s = "3+3+2*2+"
+# Output: 10
+# res = [3+3,4]
+# op = +
+# res = 10
