bfe3ae715572f83231efc3a2f0f9ef76fa1a9195
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-17 14:47:24 -0400

    Time: 3 ms (88.50%) | Memory: 23.2 MB (37.36%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # Questions:
+        #  can the input be empty
+        #  Are intervals for sure non-empty
+        #  can the ootput be sorted?
+
+        # Approach
+        #  Brute force: check every pairs with all other pairs (n^2, n)
+        #  Optimmized: sort with start time, compare end with start of next, if smaller modify else append (nlogn, n)
+
+        # res       = [1,3],
+        # intervals = [2,6],[8,10],[15,18]
+
+        intervals.sort(key = lambda i:i[0])         # sort by start so that the overlap are adjacent 
+        res = [intervals[0]]                        # add the first with smallest start interval
+        for start, end in intervals[1:]:            # iterate over all next intervals
+            if start <= res[-1][1]:                 # Start <= End of Last interval in res. ie there's a overlap
+                res[-1][1] = max(res[-1][1],end)    # check max interval ending time
+            else:
+                res.append([start,end])             # append if no overlap
+        return res
