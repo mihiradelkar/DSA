80dd63b50a4b27a7b4c3d7fe207e72d5bc0a0205
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-17 19:52:46 -0400

    Time: 63 ms (43.92%) | Memory: 22.2 MB (73.36%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,32 +1,32 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        # zeros = 0
-        # left = 0
-        # max_window = 0
-        # # [1,1,1,0,0,0,1,1,1,1,0]
-        # #            l
-        # #                      r
-        # for right in range(len(nums)):
-        #     if nums[right]==0:
-        #         zeros+=1
-        #     while zeros>k:
-        #         if nums[left]==0:
-        #             zeros-=1
-        #         left+=1
-        #     max_window = max(max_window,right-left+1)
-        # return max_window
-        if not nums: return 0
-        left = 0
-        right = 0
         zeros = 0
-        count = 0
-        while right<len(nums):
-            if nums[right] == 0:
+        left = 0
+        max_window = 0
+        # [1,1,1,0,0,0,1,1,1,1,0]
+        #            l
+        #                      r
+        for right in range(len(nums)):
+            if nums[right]==0:
                 zeros+=1
             while zeros>k:
-                if nums[left] == 0:
+                if nums[left]==0:
                     zeros-=1
                 left+=1
-            count = max(count,right-left+1)
-            right+=1
-        return count
+            max_window = max(max_window,right-left+1)
+        return max_window
+        # if not nums: return 0
+        # left = 0
+        # right = 0
+        # zeros = 0
+        # count = 0
+        # while right<len(nums):
+        #     if nums[right] == 0:
+        #         zeros+=1
+        #     while zeros>k:
+        #         if nums[left] == 0:
+        #             zeros-=1
+        #         left+=1
+        #     count = max(count,right-left+1)
+        #     right+=1
+        # return count
