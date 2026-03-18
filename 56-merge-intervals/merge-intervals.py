192f617f4276a7bd003ff669b917cb5036f0b8ae
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-18 16:54:45 -0400

    Time: 5 ms (79.52%) | Memory: 23.5 MB (7.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,5 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda x:x[0])
+        merged = [intervals[0]]
+        for start,end in intervals[1:]:
+            if start<=merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1],end)
+            else:
+                merged.append([start,end])
+        return merged
+        
+
         # Questions:
         #  can the input be empty
         #  Are intervals for sure non-empty
@@ -12,11 +23,11 @@ class Solution:
         # res       = [1,3],
         # intervals = [2,6],[8,10],[15,18]
 
-        intervals.sort(key = lambda i:i[0])         # sort by start so that the overlap are adjacent 
-        res = [intervals[0]]                        # add the first with smallest start interval
-        for start, end in intervals[1:]:            # iterate over all next intervals
-            if start <= res[-1][1]:                 # Start <= End of Last interval in res. ie there's a overlap
-                res[-1][1] = max(res[-1][1],end)    # check max interval ending time
-            else:
-                res.append([start,end])             # append if no overlap
-        return res
+        # intervals.sort(key = lambda i:i[0])         # sort by start so that the overlap are adjacent 
+        # res = [intervals[0]]                        # add the first with smallest start interval
+        # for start, end in intervals[1:]:            # iterate over all next intervals
+        #     if start <= res[-1][1]:                 # Start <= End of Last interval in res. ie there's a overlap
+        #         res[-1][1] = max(res[-1][1],end)    # check max interval ending time
+        #     else:
+        #         res.append([start,end])             # append if no overlap
+        # return res
