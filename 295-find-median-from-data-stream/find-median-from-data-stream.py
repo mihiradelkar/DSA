32c0124933230402202d6e0afd51f9b404f1d04d
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-18 09:32:27 -0400

    Time: 191 ms (48.83%) | Memory: 43.1 MB (12.70%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,20 +1,33 @@
 class MedianFinder:
 
     def __init__(self):
-        self.nums = []
+        # self.nums = []
+        self.lower = []
+        self.upper = []
         
 
     def addNum(self, num: int) -> None:
-        self.nums.append(num)
-        
+        # self.nums.append(num)
+        heapq.heappush(self.lower,-num)
+
+        if self.upper and (-self.lower[0]>self.upper[0]):
+            heapq.heappush(self.upper,-heapq.heappop(self.lower))
+
+        if len(self.lower)<len(self.upper):
+            heapq.heappush(self.lower,-heapq.heappop(self.upper))
+        elif len(self.lower)>len(self.upper)+1:
+            heapq.heappush(self.upper,-heapq.heappop(self.lower))
 
     def findMedian(self) -> float:
-        self.nums.sort()
-        n = len(self.nums)
-        if n%2 == 1:
-            return float(self.nums[n//2])
-        return (self.nums[(n//2)-1] + self.nums[n//2])/2.0
-        
+        # self.nums.sort()
+        # n = len(self.nums)
+        # if n%2 == 1:
+        #     return float(self.nums[n//2])
+        # return (self.nums[(n//2)-1] + self.nums[n//2])/2.0
+        if len(self.lower)>len(self.upper):
+            return float(-self.lower[0])
+        return (-self.lower[0] + self.upper[0])/2.0
+
 
 
 # Your MedianFinder object will be instantiated and called as such:
