74af3d38b1d24b32d0959a5ca885fe9e4ef5910b
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-18 09:39:27 -0400

    Time: 130 ms (94.86%) | Memory: 42.1 MB (54.32%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -8,15 +8,17 @@ class MedianFinder:
 
     def addNum(self, num: int) -> None:
         # self.nums.append(num)
-        heapq.heappush(self.lower,-num)
-
-        if self.upper and (-self.lower[0]>self.upper[0]):
-            heapq.heappush(self.upper,-heapq.heappop(self.lower))
-
-        if len(self.lower)<len(self.upper):
-            heapq.heappush(self.lower,-heapq.heappop(self.upper))
-        elif len(self.lower)>len(self.upper)+1:
-            heapq.heappush(self.upper,-heapq.heappop(self.lower))
+        # heapq.heappush(self.lower,-num)
+        # if self.upper and (-self.lower[0]>self.upper[0]):
+        #     heapq.heappush(self.upper,-heapq.heappop(self.lower))
+        # if len(self.lower)<len(self.upper):
+        #     heapq.heappush(self.lower,-heapq.heappop(self.upper))
+        # elif len(self.lower)>len(self.upper)+1:
+        #     heapq.heappush(self.upper,-heapq.heappop(self.lower))
+        if len(self.upper) == len(self.lower):
+            heapq.heappush(self.upper, -heapq.heappushpop(self.lower, -num))
+        else:
+            heapq.heappush(self.lower, -heapq.heappushpop(self.upper, num))
 
     def findMedian(self) -> float:
         # self.nums.sort()
@@ -24,9 +26,12 @@ class MedianFinder:
         # if n%2 == 1:
         #     return float(self.nums[n//2])
         # return (self.nums[(n//2)-1] + self.nums[n//2])/2.0
-        if len(self.lower)>len(self.upper):
-            return float(-self.lower[0])
-        return (-self.lower[0] + self.upper[0])/2.0
+        # if len(self.lower)>len(self.upper):
+        #     return float(-self.lower[0])
+        # return (-self.lower[0] + self.upper[0])/2.0
+        if len(self.lower)==len(self.upper):
+            return (-self.lower[0] + self.upper[0])/2.0
+        return float(self.upper[0])
 
 
 
