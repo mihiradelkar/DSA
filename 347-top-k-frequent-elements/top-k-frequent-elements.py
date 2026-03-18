b2a3df999f1cc651a31c722511be9670bc5000ce
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-17 22:04:37 -0400

    Time: 3 ms (88.72%) | Memory: 23 MB (55.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # count = Counter(nums)
-        # heap = []
-        # for num,freq in count.items():
-        #     heapq.heappush(heap,(freq,num))
-        #     if len(heap)>k:
-        #         heapq.heappop(heap)
-        # return [num for _,num in heap]
-
+        
+        # heap
         count = Counter(nums)
-        res = []
-        bucket = [[]for _ in range(len(nums)+1)]
+        heap = []
         for num,freq in count.items():
-            bucket[freq].append(num)
-        for freq in range(len(bucket)-1,0,-1):
-            for num in bucket[freq]:
-                res.append(num)
-                if len(res)==k:
-                    return res
+            heapq.heappush(heap,(freq,num))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return [num for _,num in heap]
+
+        # bucket sort
+        # count = Counter(nums)
+        # bucket = [[]for _ in range(len(nums)+1)]
+        # for num,freq in count.items():
+        #     bucket[freq].append(num)
+        # res = []
+        # for freq in range(len(bucket)-1,0,-1):
+        #     for num in bucket[freq]:
+        #         res.append(num)
+        #         if len(res)==k:
+        #             return res
 
