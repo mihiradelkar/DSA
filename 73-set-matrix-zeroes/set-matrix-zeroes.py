322cc2d2e0baa69dc0658916aacf1057a11ae41c
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-21 08:36:41 -0400

    Time: 7 ms (28.25%) | Memory: 20.2 MB (29.67%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,15 +5,44 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
-        rows = set()
-        cols = set()
-        for row in range(m):
-            for col in range(n):
-                if matrix[row][col]==0:
-                    rows.add(row)
-                    cols.add(col)
+        # rows = set()
+        # cols = set()
+        # for row in range(m):
+        #     for col in range(n):
+        #         if matrix[row][col]==0:
+        #             rows.add(row)
+        #             cols.add(col)
+        
+        # for row in range(m):
+        #     for col in range(n):
+        #         if row in rows or col in cols:
+        #             matrix[row][col]=0
+
+        first_row = False
+        first_col = False
         
         for row in range(m):
             for col in range(n):
-                if row in rows or col in cols:
+                if matrix[row][col]==0:     # if 0 found
+                    if row == 0:            # is it in the first row?
+                        first_row = True    # flag to make entire row 0
+                    if col == 0:            # is it in the first col?
+                        first_col = True    # flag to make entire col 0
+                    matrix[row][0]=0        # make 0 in first row
+                    matrix[0][col]=0        # make 0 in first col
+        
+        # make the inner box values 0 if 0th row/col has a 0 
+        for row in range(1,m):
+            for col in range(1,n):
+                if matrix[row][0]==0 or matrix[0][col]==0:
                     matrix[row][col]=0
+        
+        # if first row had a 0 iterate over 1st col to make entire col 0
+        if first_row:
+            for col in range(n):
+                matrix[0][col]=0
+        
+        # if first col had a 0 iterate over 1st row to make entire row 0
+        if first_col:
+            for row in range(m):
+                matrix[row][0]=0
