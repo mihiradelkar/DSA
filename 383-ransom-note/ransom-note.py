e2c37636c00852ca2779c0c39b6fbb580a6a6c90
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-27 10:46:44 -0400

    Time: 15 ms (76.60%) | Memory: 19.7 MB (25.31%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count_m = Counter(magazine)
+
+        for ch in ransomNote:
+            if ch in count_m and count_m[ch]>0:
+                count_m[ch]-=1
+            else:
+                return False
+        return True
+        
