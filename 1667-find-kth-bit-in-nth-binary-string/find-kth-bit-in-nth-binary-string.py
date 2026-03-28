0351a7edb9e879d891dfe926a97dcac061b6527d
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-28 18:01:14 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (79.93%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,11 +1,32 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        s_prev = "0"
-        if n==1:
-            return s_prev
-        for i in range(1,n):
-            inverted = "".join("1" if bit == "0" else "0" for bit in s_prev)
-            s_curr = s_prev + "1" + inverted[::-1]
-            s_prev=s_curr
-        return s_prev[k-1]
+        # s_prev = "0"
+        # if n==1:
+        #     return s_prev
+        # for i in range(1,n):
+        #     inverted = "".join("1" if bit == "0" else "0" for bit in s_prev)
+        #     s_prev = s_prev + "1" + inverted[::-1]
+        # return s_prev[k-1]
+
+        if n == 1:
+            return "0"
+        # 1 << 1  =  10      (binary) = 2
+        # 1 << 2  =  100     (binary) = 4
+        # 1 << n  =  2^n
+        length = (1<<n)-1
+        # left and right partition seperated by 1
+        mid = (length//2)+1
+        #  if k is mid then always "1"
+        if k == mid:
+            return "1"
+        #  k on left side find kth bit on left
+        elif k<mid:
+            return self.findKthBit(n-1,k)
+        #  k on right side find total - k +1 bit on left
+        # findKthBit(3,5): length=7, mid=4, k>mid → mirrored=7-5+1=3
+        else:
+            mirrored = length-k+1
+            bit = self.findKthBit(n-1,mirrored)
+            return "1" if bit == "0" else "0" 
+
         
