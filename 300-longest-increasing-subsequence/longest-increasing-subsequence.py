615f8839dc039de47fac7f5f0ddffa157b441728
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-31 08:47:30 -0400

    Time: 3 ms (94.97%) | Memory: 19.4 MB (73.11%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -21,22 +21,23 @@ class Solution:
         # [10][9][2][2,5][2,3][2,3,7][2,3,7,101][2,3,7,18]
 
         tails = []
-        def binary_search_lower(target):
-            start = 0
-            end = len(tails)
-            while start<end:
-                mid = (start+end)//2
-                if tails[mid]<target:
-                    start = mid+1
-                else:
-                    end = mid
-            return start
+        # def binary_search_lower(target):
+        #     start = 0
+        #     end = len(tails)
+        #     while start<end:
+        #         mid = (start+end)//2
+        #         if tails[mid]<target:
+        #             start = mid+1
+        #         else:
+        #             end = mid
+        #     return start
 
         for num in nums:
-            pos =  binary_search_lower(num)
+            # pos =  binary_search_lower(num)
+            pos = bisect_left(tails, num)
             if pos==len(tails):
                 tails.append(num)
             else:
                 tails[pos] = num
-        print(tails)
+        # print(tails)
         return len(tails)
