3c6ada8428636bb5f95a02dfe3a9119043bb98fe
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-03-31 08:43:09 -0400

    Time: 11 ms (77.67%) | Memory: 19.6 MB (25.77%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -9,13 +9,13 @@ class Solution:
         # [2,5,7,101] [2,5,7,18] [2,3,7,101] [2,3,7,18]
         # [9,101] [9,18]
         # [10,101] [10,18]
-        n = len(nums)
-        longest = [1] * n
-        for i in range(n-1,-1,-1):
-            for j in range(i+1,n):
-                if nums[i]<nums[j]:
-                    longest[i] = max(longest[i],1+longest[j])
-        return max(longest)
+        # n = len(nums)
+        # longest = [1] * n
+        # for i in range(n-1,-1,-1):
+        #     for j in range(i+1,n):
+        #         if nums[i]<nums[j]:
+        #             longest[i] = max(longest[i],1+longest[j])
+        # return max(longest)
 
         # [10,9,2,5,3,7,101,18]
         # [10][9][2][2,5][2,3][2,3,7][2,3,7,101][2,3,7,18]
@@ -34,8 +34,9 @@ class Solution:
 
         for num in nums:
             pos =  binary_search_lower(num)
-            if pos<len(tails):
-                tails[pos] = min(tails[pos],num)
-            else:
+            if pos==len(tails):
                 tails.append(num)
+            else:
+                tails[pos] = num
+        print(tails)
         return len(tails)
