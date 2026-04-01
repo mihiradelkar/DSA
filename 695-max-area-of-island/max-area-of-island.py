5768d2cc97968324cc834d5f07af9722943c2f6b
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-01 09:17:53 -0400

    Time: 15 ms (86.05%) | Memory: 19.3 MB (99.48%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        def bfs(row,col):
+            area = 0
+            queue = deque([(row,col)])
+            directions = [(0,1),(1,0),(-1,0),(0,-1)]
+            while queue:
+                r, c = queue.popleft()
+                # print(r,c)
+                if grid[r][c]==0:
+                    continue
+                grid[r][c]=0
+                area+=1
+                for dr, dc in directions:
+                    nr = r+dr
+                    nc = c+dc
+                    if 0<=nr<m and 0<=nc<n and grid[nr][nc]==1:
+                        queue.append((nr,nc))
+            return area
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    max_area = max(max_area,bfs(i,j))
+        return max_area
+        
