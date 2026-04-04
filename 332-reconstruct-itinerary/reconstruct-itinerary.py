997eb309c5a4e7bd476b5f95dc760db83b5923da
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-04 09:00:46 -0400

    Time: 3 ms (74.87%) | Memory: 19.8 MB (70.46%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -2,8 +2,15 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         airport = defaultdict(list)
         # print(sorted(tickets, reverse=True))
-        for src, dst in sorted(tickets, reverse=True):
+        # for src, dst in sorted(tickets, reverse=True):
+        # Adj list
+        for src, dst in tickets:
             airport[src].append(dst)
+        # sort dst list for smaller lexical order 
+        for src in airport.keys():
+            airport[src].sort(reverse=True)
+        # print(airport)
+        #  post order dfs from JFK
         res = []
         def dfs(src):
             while airport[src]:
