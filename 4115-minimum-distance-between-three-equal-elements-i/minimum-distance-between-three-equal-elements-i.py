5e2317f6359cc77b05c8383759b16693ac0de80f
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-09 23:53:07 -0400

    Time: 3 ms (67.11%) | Memory: 19.2 MB (95.64%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -6,7 +6,9 @@ class Solution:
         # print(good)
         res = float("inf")
         for v in good.values():
-            for i in range(2,len(v)):
-                total = abs(v[i-2] - v[i-1]) + abs(v[i-1] - v[i]) + abs(v[i] - v[i-2])
+            # for i in range(2,len(v)):
+                # total = abs(v[i-2] - v[i-1]) + abs(v[i-1] - v[i]) + abs(v[i] - v[i-2])
+            for i in range(len(v)-2):
+                total = 2 * (v[i+2]-v[i]) # = (b - a) + (c - b) + (c - a)   ← all non-negative since a ≤ b ≤ c
                 res = min(res,total)
         return res if res != float("inf") else -1
