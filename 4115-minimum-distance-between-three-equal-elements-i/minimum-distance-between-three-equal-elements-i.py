b53720c6a29500fb02f4ea9934a4b3b340f09cfe
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-09 23:08:25 -0400

    Time: 3 ms (67.11%) | Memory: 19.5 MB (16.11%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        good = defaultdict(list)
+        for i, num in enumerate(nums):
+            good[num].append(i)
+        # print(good)
+        res = float("inf")
+        for v in good.values():
+            for i in range(2,len(v)):
+                total = abs(v[i-2] - v[i-1]) + abs(v[i-1] - v[i]) + abs(v[i] - v[i-2])
+                res = min(res,total)
+        return res if res != float("inf") else -1
