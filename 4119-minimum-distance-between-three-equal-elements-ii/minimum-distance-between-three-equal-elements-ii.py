136fcad1a61ce943da2ed9d954dc10885478ca07
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-11 01:02:40 -0400

    Time: 291 ms (89.84%) | Memory: 50.8 MB (48.13%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -5,11 +5,11 @@ class Solution:
             good[num].append(i)
         best = float("inf")
         for v in good.values():
-            if len(v)<3:
-                continue
+            if len(v)>=3:
+                # continue
             # b-a + c-b + c-a = 2(c-a)
-            for i in range(len(v)-2):
-                best = min(best,(2*(v[i+2]-v[i])))
+                for i in range(len(v)-2):
+                    best = min(best,(2*(v[i+2]-v[i])))
         return best if best != float("inf") else -1
 
         
