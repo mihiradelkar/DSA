57bb54d91dafb407755e7c5f2c059651f619ba08
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-12 09:47:01 -0400

    Time: 214 ms (50.55%) | Memory: 57.3 MB (40.76%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,24 +1,23 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        meetings.sort()
-        available = [i for i in range(n)]
-        used = [] #(end,room)
-        count = [0] * n
+        meetings.sort()                                             # IMPORTANT
+        available = [i for i in range(n)]                   # sorted room no need to heapify
+        used = []  #(end,room)
+        count = [0] * n 
         for start, end in meetings:
             # finish all meeting with end less than cur start
             while used and start>=used[0][0]:
                 _,room = heapq.heappop(used)
                 heapq.heappush(available,room)
-            # if meeeting rooms are available
-            if available:
-                room = heapq.heappop(available)
-                heapq.heappush(used,(end,room))
-                count[room] +=1
             # if meeting are not available
-            else:
-                last_end,room = heapq.heappop(used)
-                heapq.heappush(used,(last_end+(end-start),room))
-                count[room] +=1
+            if not available:
+                low_end,room = heapq.heappop(used)
+                end = low_end+(end-start)
+                heapq.heappush(available,room)
+            # if meeeting rooms are available
+            room = heapq.heappop(available)
+            heapq.heappush(used,(end,room))
+            count[room] +=1
 
         # print(count)
         return count.index(max(count))
