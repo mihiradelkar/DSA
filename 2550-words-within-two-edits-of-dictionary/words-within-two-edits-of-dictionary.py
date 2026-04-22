837d34036af3ac0e6fa1f5dc422af76e0c2f65ef
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-22 08:27:57 -0400

    Time: 7 ms (96.57%) | Memory: 19.4 MB (50.49%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -1,29 +1,42 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        def pattern(word):
-            res = []
-            n = len(word)
-            for i in range(n):
-                for j in range(i,n):
-                    w = list(word)
-                    w[i] = "*"
-                    w[j] = "*"
-                    res.append("".join(w))
-            return res
+        # def pattern(word):
+        #     res = []
+        #     n = len(word)
+        #     for i in range(n):
+        #         for j in range(i,n):
+        #             w = list(word)
+        #             w[i] = "*"
+        #             w[j] = "*"
+        #             res.append("".join(w))
+        #     return res
                     
-        pm = set()
-        for word in dictionary:
-            for p in pattern(word):
-                pm.add(p)
+        # pm = set()
+        # for word in dictionary:
+        #     for p in pattern(word):
+        #         pm.add(p)
         
-        res =[]
-        for word in queries:
-            # for p in pattern(word):
-                # if p in pm:
-                #     res.append(word)
-                #     break
-            if any(p in pm for p in pattern(word)):   # short circuits on first hit
-                res.append(word)
+        # res =[]
+        # for word in queries:
+        #     # for p in pattern(word):
+        #         # if p in pm:
+        #         #     res.append(word)
+        #         #     break
+        #     if any(p in pm for p in pattern(word)):   # short circuits on first hit
+        #         res.append(word)
+        # return res
 
+        res = []
+        for query in queries:
+            for word in dictionary:
+                diff = 0
+                for i in range(len(word)):
+                    if query[i] != word[i]:
+                        diff+=1
+                    if diff>2:
+                        break
+                if diff <= 2:
+                    res.append(query)
+                    break
         return res
-        
+ 
