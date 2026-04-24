9c4028043641dc740254fdd686937c74580e6890
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-23 21:44:51 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.15%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.py
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.py
@@ -4,10 +4,10 @@ class Solution:
         # -10 -9 -8 -7 -6 -5 -4 -3 -2 -1  0  1  2  3  4  5  6  7  8  9  10
         pos = 0
         free = 0
-        for ch in moves:
-            if ch == "L":
+        for m in moves:
+            if m == "L":
                 pos-=1
-            elif ch == "R":
+            elif m == "R":
                 pos+=1
             else:
                 free+=1
