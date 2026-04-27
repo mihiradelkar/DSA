6f7a897922343d8855b42c0d0e71ccee7414bec8
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-04-27 10:27:42 -0400

    Time: 201 ms (55.97%) | Memory: 34.2 MB (77.24%) - LeetSync

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,37 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        # [[2,4,3],
+        #  [6,5,2]]
+
+        direction_map = {
+            1:[(0,1),(0,-1)],
+            2:[(1,0),(-1,0)],
+            3:[(0,-1),(1,0)],
+            4:[(0,1),(1,0)],
+            5:[(0,-1),(-1,0)],
+            6:[(0,1),(-1,0)],
+        }
+        m = len(grid)
+        n = len(grid[0])
+        queue = deque([(0,0)])
+        visited = set([(0,0)])
+        
+        while queue:
+            r,c = queue.popleft()
+            if r==m-1 and c ==n-1:
+                return True
+            st = grid[r][c]
+            # check what cells can we go
+            for dr,dc in direction_map[st]:
+                nr,nc = r+dr, c+dc
+                if 0<=nr<m and 0<=nc<n and (nr,nc) not in visited:
+                    # print(nr,nc)
+                    # check if connected
+                    for rdr, rdc in direction_map[grid[nr][nc]]:
+                        rnr,rnc = nr+rdr, nc+rdc
+                        # print(rnr,rnc)
+                        if r == rnr and c==rnc:
+                            queue.append((nr,nc))
+                            visited.add((nr,nc))
+                            break
+        return False
