fb8492022e0718d588990b75d204d41a26796bf8
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-09 12:26:23 -0400

    Time: 115 ms (71.43%) | Memory: 19.8 MB (2.86%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
@@ -0,0 +1,40 @@
+class Solution:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        # m = len(grid)
+        # n = len(grid[0])
+        # p = 2*(m+n)-4
+        # k= k%p
+        # layers = defaultdict(list)
+        # for i in range(m):
+        #     for j in range(n):
+        #         layer = min(i%m, j%n)
+        #         layers[layer].append(grid[i][j])
+        # print(layers)
+        # return grid
+
+        m = len(grid)
+        n = len(grid[0])
+        for layer in range(min(m,n)//2):
+            r1,c1 = layer, layer
+            r2,c2 = m-1-layer,n-1-layer
+            ring = []
+            for c in range(c1,c2): ring.append(grid[r1][c])
+            for r in range(r1,r2): ring.append(grid[r][c2])
+            for c in range(c2,c1,-1): ring.append(grid[r2][c])
+            for r in range(r2,r1,-1): ring.append(grid[r][c1])
+            l = len(ring)
+            shift = k % l
+            # print(ring)
+            # print(shift)
+            ring = ring[shift:] + ring[:shift] 
+            # print(ring)
+
+            idx = 0
+            for c in range(c1,c2):      grid[r1][c] = ring[idx]; idx+=1
+            for r in range(r1,r2):      grid[r][c2] = ring[idx]; idx+=1
+            for c in range(c2,c1,-1):   grid[r2][c] = ring[idx]; idx+=1
+            for r in range(r2,r1,-1):   grid[r][c1] = ring[idx]; idx+=1
+        return grid
+
+        # m+n-1+m-1+n-2
+    
