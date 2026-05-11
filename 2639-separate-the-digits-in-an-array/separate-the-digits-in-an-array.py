118d41f709f3323eeebfc96ded3f43bffb3507cd
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-11 01:37:52 -0400

    Time: 4 ms (29.13%) | Memory: 19.5 MB (49.38%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -2,8 +2,11 @@ class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         res = []
         for num in nums:
-            for d in str(num):
-                res.append(int(d))
+            if num>9:
+                for d in str(num):
+                    res.append(int(d))
+            else:
+                res.append(num)
         return res
 
         
