eaebd1279a49237bc4819a8e36b155ec2c577f7b
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-12 11:31:29 -0400

    Time: 62 ms (39.60%) | Memory: 55.6 MB (60.40%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
@@ -1,18 +1,17 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        s_task = [(a,r,r-a) for a,r in tasks]
-        s_task = sorted(s_task, key=lambda x: (-x[2],-x[1]))
-        # print(s_task)
+        tasks.sort(key=lambda x: x[0]-x[1])
         curr = 0
         used = 0
-        for a,r,_ in s_task:
-            # print(a,r)
-            add = max(0, r - curr)
-            curr+=add
-            used+=add
-            curr-=a
-        # print(curr)
+        for a, r in tasks:
+            add = max(0, r - curr)   # ← fix here
+            curr += add
+            used += add
+            curr -= a
         return used
+
+
+
         # s_task = [(a,r,r-a) for a,r in tasks]
         # s_task = sorted(s_task, key=lambda x: (x[2],x[1]))
         # # print(s_task)
