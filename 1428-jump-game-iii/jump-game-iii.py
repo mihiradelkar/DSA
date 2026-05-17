625bec232951538121f076a5eb4bfa7c4e734c63
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-17 10:25:33 -0400

    Time: 11 ms (65.93%) | Memory: 26.4 MB (80.96%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        # n = len(arr)
+        # jumps = defaultdict(list)
+        # target = None
+        # for i, num in enumerate(arr):
+        #     if num == 0:
+        #         target = i
+        #     if i+num < n:
+        #         jumps[i].append(i+num)
+        #     if i-num >= 0:
+        #         jumps[i].append(i-num)
+        # # print(jumps)
+        # if not target:
+        #     return False
+        # queue = deque([start])
+        # visited = set()
+        # while queue:
+        #     node = queue.popleft()
+        #     if target == node:
+        #         return True
+        #     for j in jumps[node]:
+        #         if j not in visited:
+        #             queue.append(j)
+        #             visited.add(j)
+        # return False
+
+        n = len(arr)
+        queue = deque([start])
+        visited = {start}
+        while queue:
+            i = queue.popleft()
+            if 0 == arr[i]:
+                return True
+            for j in [i+arr[i], i-arr[i]]:
+                if 0<=j<n and j not in visited:
+                    queue.append(j)
+                    visited.add(j)
+        return False
+        
