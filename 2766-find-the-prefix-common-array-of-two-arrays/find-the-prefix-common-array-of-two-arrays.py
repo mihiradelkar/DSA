c9fca1fbd30bc2f9921c1e0f376665f9e259ff4b
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-20 02:29:33 -0400

    Time: 8 ms (55.50%) | Memory: 19.1 MB (93.97%) - LeetSync

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        # A = [1,3,2,4], 
+        # B = [3,1,2,4]
+        # C = [0,2,3,4]
+        n = len(A)
+        C = [0] * n
+        mapp = defaultdict(int)
+        for i in range(n):
+            mapp[A[i]]=max(mapp[A[i]],i)
+            mapp[B[i]]=max(mapp[B[i]],i)
+        # print(mapp)
+        for i in mapp.values():
+            C[i] += 1 
+        for i in range(1,n):
+            C[i]+= C[i-1]
+        return C
