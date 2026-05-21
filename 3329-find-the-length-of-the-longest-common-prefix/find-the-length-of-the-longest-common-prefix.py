8e5d0638dd8ebd3be76233779e12ed22bba57e90
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-21 11:06:12 -0400

    Time: 339 ms (52.49%) | Memory: 31.1 MB (51.32%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,27 +1,44 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
+# class TrieNode:
+#     def __init__(self):
+#         self.children = {}
 
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        root1 = TrieNode()
+        # HASH SET APPROACH
+        prefix = set()
         for num in arr1:
-            node = root1
-            for d in str(num):
-                node.children.setdefault(d,TrieNode())
-                node =  node.children[d]
-        # print(root1)
-
+            s = str(num)
+            for i in range(1,len(s)+1):
+                prefix.add(s[:i])
+        # print(prefix)
         max_count = 0
         for num in arr2:
-            count = 0
-            node = root1
-            for d in str(num):
-                if d in node.children:
-                    count+=1
-                    node =  node.children[d]
+            s = str(num)
+            for i in range(1,len(s)+1):
+                if s[:i] in prefix:
+                    max_count = max(i, max_count)
                 else:
                     break
-            max_count = max(count, max_count)
         return max_count
         
+        # TRIE APPROACH
+        # root1 = TrieNode()
+        # for num in arr1:
+        #     node = root1
+        #     for d in str(num):
+        #         node.children.setdefault(d,TrieNode())
+        #         node =  node.children[d]
+        # # print(root1)
+
+        # max_count = 0
+        # for num in arr2:
+        #     count = 0
+        #     node = root1
+        #     for d in str(num):
+        #         if d in node.children:
+        #             count+=1
+        #             node =  node.children[d]
+        #         else:
+        #             break
+        #     max_count = max(count, max_count)
+        # return max_count
