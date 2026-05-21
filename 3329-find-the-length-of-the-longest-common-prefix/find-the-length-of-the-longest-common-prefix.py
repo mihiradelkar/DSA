44a21d6b624a2050ae3f61f0687b70af6799bf2a
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-21 11:13:19 -0400

    Time: 168 ms (88.56%) | Memory: 30 MB (71.07%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -4,23 +4,39 @@
 
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        # HASH SET APPROACH
+        # WITHOUT STR CONVERSION
         prefix = set()
         for num in arr1:
-            s = str(num)
-            for i in range(1,len(s)+1):
-                prefix.add(s[:i])
+            while num>0:
+                prefix.add(num)
+                num//=10
         # print(prefix)
-        max_count = 0
+        max_num = 0
         for num in arr2:
-            s = str(num)
-            for i in range(1,len(s)+1):
-                if s[:i] in prefix:
-                    max_count = max(i, max_count)
-                else:
+            while num>max_num:
+                if num in prefix:
+                    max_num = num
                     break
-        return max_count
-        
+                num//=10
+        return len(str(max_num)) if max_num else 0
+
+        # HASH SET APPROACH
+        # prefix = set()
+        # for num in arr1:
+        #     s = str(num)
+        #     for i in range(1,len(s)+1):
+        #         prefix.add(s[:i])
+        # # print(prefix)
+        # max_count = 0
+        # for num in arr2:
+        #     s = str(num)
+        #     for i in range(1,len(s)+1):
+        #         if s[:i] in prefix:
+        #             max_count = max(i, max_count)
+        #         else:
+        #             break
+        # return max_count
+
         # TRIE APPROACH
         # root1 = TrieNode()
         # for num in arr1:
