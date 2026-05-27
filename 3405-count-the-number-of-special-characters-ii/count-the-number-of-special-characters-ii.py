6774fd5ba549b2486932013893bf102b2205c3c4
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-27 02:22:38 -0400

    Time: 206 ms (82.94%) | Memory: 21.7 MB (15.88%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,15 +1,20 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        seen_lower = {}
-        seen_upper = {}
-        count=0
+        lower = {}
+        upper = {}
         for i, ch in enumerate(word):
             if ch.islower():
-                seen_lower[ch]=i
+                lower[ch]=i
             else:
-                low = ch.lower()
-                if low not in seen_upper:
-                    seen_upper[low] = i
+                if ch not in upper:
+                    upper[ch] = i
+        count=0
+        for c,i in lower.items():
+            cap = c.upper()
+            if cap in upper and upper[cap]>i:
+                count+=1
+        return count
+
         # print(seen)
         # for k,v in seen.items():
         #     # print(k,v)
@@ -17,7 +22,3 @@ class Solution:
         #     # print(cap)
         #     if cap in seen and seen[cap]>v:
         #         count+=1
-        for low,i in seen_lower.items():
-            if low in seen_upper and seen_upper[low]>i:
-                count+=1
-        return count
