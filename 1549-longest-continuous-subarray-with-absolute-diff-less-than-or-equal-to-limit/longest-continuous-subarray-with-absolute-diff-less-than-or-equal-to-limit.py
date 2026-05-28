774ac4a4ab118bb113bd5ef4643f606a9cc61b52
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-27 22:39:25 -0400

    Time: 179 ms (85.13%) | Memory: 29.9 MB (95.35%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -14,29 +14,52 @@ class Solution:
         #             break
         # return res
                     
-        n = len(nums)
-        res = 0
-        inc_q = deque()
-        dec_q = deque()
-        l = 0
-        for r, num in enumerate(nums):
-            while inc_q and nums[inc_q[-1]]>=num:
-                inc_q.pop()
-            inc_q.append(r)
+        # n = len(nums)
+        # res = 0
+        # inc_q = deque()
+        # dec_q = deque()
+        # l = 0
+        # for r, num in enumerate(nums):
+        #     while inc_q and nums[inc_q[-1]]>=num:
+        #         inc_q.pop()
+        #     inc_q.append(r)
 
-            while dec_q and nums[dec_q[-1]]<=num:
-                dec_q.pop()
-            dec_q.append(r)
+        #     while dec_q and nums[dec_q[-1]]<=num:
+        #         dec_q.pop()
+        #     dec_q.append(r)
 
-            while nums[dec_q[0]]-nums[inc_q[0]] > limit:
-                if inc_q[0] == l: inc_q.popleft()
-                if dec_q[0] == l: dec_q.popleft()
-                l+=1
+        #     while nums[dec_q[0]]-nums[inc_q[0]] > limit:
+        #         if inc_q[0] == l: inc_q.popleft()
+        #         if dec_q[0] == l: dec_q.popleft()
+        #         l+=1
             
-            # print("inc_q",inc_q)
-            # print("dec_q",dec_q)
-            # print("")
-            res = max(res,r-l+1)
-        return res
+        #     # print("inc_q",inc_q)
+        #     # print("dec_q",dec_q)
+        #     # print("")
+        #     res = max(res,r-l+1)
+        # return res
 
+        l = 0
+        max_q = deque()
+        min_q = deque()
+        res = 0
+        for r, num in enumerate(nums):
+            while max_q and max_q[-1]<num:
+                max_q.pop()
+            max_q.append(num)
+
+            while min_q and min_q[-1]>num:
+                min_q.pop()
+            min_q.append(num)
+
+            while max_q[0]-min_q[0] > limit:
+                if max_q[0] == nums[l]:
+                    max_q.popleft()
+                if min_q[0] == nums[l]:
+                    min_q.popleft()
+                l+=1
+
+            res = max(res,r-l+1)
+
+        return res 
         
