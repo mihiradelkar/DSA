2353e047434f8270b650b8d7edf792362e411fc8
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-29 09:10:09 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (87.82%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -3,7 +3,10 @@ class Solution:
         min_d = float("inf")
         for num in nums:
             s = 0
-            for d in str(num):
-                s+=int(d)
+            # for d in str(num):
+            #     s+=int(d)
+            while num:
+                num, d = divmod(num, 10)
+                s+=d
             min_d = min(min_d,s)
         return min_d
