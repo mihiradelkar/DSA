1231cf3ab53d8429ccbd1e1e6fb1f465ca81fe38
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-29 09:12:26 -0400

    Time: 3 ms (80.35%) | Memory: 19.4 MB (21.61%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        min_d = float("inf")
+        min_d = 36 # float("inf") # or 36 is the max 10^4 10000 max will be 9999 = 36
         for num in nums:
             s = 0
             # for d in str(num):
