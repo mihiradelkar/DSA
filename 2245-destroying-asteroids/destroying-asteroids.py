df47d865fa86e0c234e9d9eefc4d45afe1dbc5ee
Mihir Adelkar <adelkar.m@northeastern.edu>
2026-05-31 09:14:29 -0400

    Time: 71 ms (73.70%) | Memory: 33.9 MB (75.43%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,11 +1,9 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         asteroids.sort()
-        new_mass = mass
         for a in asteroids:
-            # print(a,new_mass)
-            if new_mass >= a:
-                new_mass+=a
+            if mass >= a:
+                mass+=a
             else:
                 return False
         return True
